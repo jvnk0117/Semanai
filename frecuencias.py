ea5e604cbd0c6080225564893e54ba6b69b45218
Edumu <a01745219@itesm.mx>
2019-10-28 12:06:51 -0500

    no le hagan caso

---
+++ frecuencias.py
@@ -0,0 +1,57 @@
+def obtenerTexto(archivo):
+    entrada = open(archivo, 'r')
+    lineas = entrada.readlines()
+    lineas2 = []
+    for linea in lineas:
+        x = linea.split('\n')
+        if '' in x:
+            x.remove('')
+        if len(x)>0:
+            lineas2.append(x[0])
+    return lineas2
+
+
+def depurarLetras(texto):
+    letrasDepuradas = []
+    for frase in texto:
+        palabras = frase.split(' ')
+        for palabra in palabras:
+            plabra_list = palabra.split()
+            for n in plabra_list:
+                letras_list = n.split(',')
+                if '' in letras_list:
+                    letras_list.remove('')
+                for j in letras_list:
+                    letras = list(j)
+                    if '.' in letras:
+                        letras.remove('.')
+                    if ',' in letras:
+                        letras.remove(',')
+                    if '(' in letras:
+                        letras.remove('(')
+                    if ')' in letras:
+                        letras.remove(')')
+                    if '“' in letras:
+                        letras.remove('“')
+                    if '”' in letras:
+                        letras.remove('”')
+                    for k in letras:
+                        letrasDepuradas.append(k)
+    return letrasDepuradas
+
+
+def frecuenciaLetras(texto):
+    frequence = {}
+    listaLetras = depurarLetras(texto)
+
+
+
+
+frecuenciaLetras(obtenerTexto('Text2.txt'))
+def frecuencias():
+    texto1 = obtenerTexto('Text1.txt')
+    texto2 = obtenerTexto('Text2.txt')
+    texto3 = obtenerTexto('Text3.txt')
+    texto1_letras = frecuenciaLetras(texto1)
+    texto2_letras = frecuenciaLetras(texto2)
+    texto3_letras = frecuenciaLetras(texto3)
