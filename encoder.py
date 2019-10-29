9d423cadaf86aed6e53f8647e27ffbe0b0a9ce0b
Alejandro <alejandro-perezg>
2019-10-29 13:53:25 -0600

    Agregada funcion para leer files como strings

--- encoder.py
+++ encoder.py
@@ -13,7 +13,8 @@ def encode(text, s):
             #Evita que lea el valor unicode de un espacio y lo pon
             crypt += " "
         else:
-            crypt += chr((ord(char) + s - 97) % 26 + 97)
+            crypt += chr((ord(
+                char) + s - 97) % 26 + 97)
 
     #Transforma en minuscula
     return crypt.lower()
@@ -22,7 +23,16 @@ def encode(text, s):
 
 
 def main():
-    print("texto: "+text)
-    print("Decode: ")
-    print(encode(text,swap))
+
+    with open('Text1.txt', 'r', encoding="utf-8") as file: #Ayuda a leer un file como si fuese un string
+        data = file.read().replace('\n', '')
+
+    print("Original: \n" +data)
+    print("///////////////////////////////////////////////////////////////")
+
+    print("Decoded: \n" + encode(data,3))
+
+if __name__ == '__main__':
+
+    main()
 
