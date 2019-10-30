298b010b5902a23da788199aa13eab5e8b9bd777
Sebastian Morales Martin <sebastianmoralesm1999@outlook.com>
2019-10-30 12:04:07 -0600

    copy pasta ENGLISH

--- jiCuadrada.py
+++ jiCuadrada.py
@@ -7,6 +7,9 @@
 import encoder
 import frecuencias
 
+ABECEDARIO = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u",
+              "v", "w", "x", "y", "z"]
+
 dicJI = {}
 
 def bruteForce():
@@ -19,3 +22,14 @@ def bruteForce():
             print("ATTEMPT %i: %s (INITIAL VALUE)" % (i, output))
         else:
             print("ATTEMPT %i: %s " %(i, output))
+
+def computeJiSquared(encodedText, sampleText):
+    for i in range(1, 26+1):    #For cycle from the first letter to last
+        encodedTextDictionary = frecuencias.frecuenciaLetras(encodedText)
+        sampleTextDictionary = frecuencias.frecuenciaLetras(sampleText)
+        for n in range(len(ABECEDARIO)):
+            charToProcess = ABECEDARIO[n]
+            charEncodedText = encodedTextDictionary.get(charToProcess)
+            charSampleText = sampleTextDictionary.get(charToProcess)
+            ji = ((charEncodedText - charSampleText)^2)/charSampleText
+            dicJI[n] = ji
