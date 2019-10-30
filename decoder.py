ea957509105734c6324cf77e6e1ca1404ff2b8cd
v1v1 <a01374461@itesm.mx>
2019-10-30 11:13:56 -0600

    Arreglar encode para que leyera archivos sin llamr a un main

--- decoder.py
+++ decoder.py
@@ -1,35 +1,28 @@
 import identificarIndice
-import encoder as e
+import encoder
 
 sampleText = open("Text2.txt", "r", encoding="utf8")  # abrir txt de idioma.
 
 
-def decoder():
-    sampleText = open("Text2.txt","r", encoding="utf8") #abrir txt de idioma.
-    toDecode = open("Text1.txt","r", encoding="utf8") #abrir txt de idioma.
+def decoder(toDecode, sampleText):
     # sacar indices mas altos.
     indexToDecod = identificarIndice.identificarIndice(toDecode)
     indexIdiom = identificarIndice.identificarIndice(sampleText)
 
     deltaIndex = indexIdiom - indexToDecod
+
     print(deltaIndex)
-    decoded = e.encode(toDecode,-deltaIndex)
-
-    sampleText.close()
-    toDecode.close()
-
+    #Desecriptar mensaje usando la llave.
+    decoded = encoder.encode(toDecode,-deltaIndex)
     return (decoded)
 
-print(sampleText)
-#print(e.encode(sampleText,4))
 
-#if __name__ == '__main__':
-    #textToDecode = open("Text2.txt", 'r', encoding="utf8")
-    #textSample = open("Text2.txt", 'r', encoding="utf8")
-   #text = open("Text1.txt", "r", encoding="utf8")  # abrir txt de idioma.
+if __name__ == '__main__':
+    textToDecode = open("textoEncriptado.txt", 'r', encoding="utf8")
+    textSample = open("Text2.txt", 'r', encoding="utf8")
 
-   # e.encode(text,4)
-    #decodedText = decoder()
-    #print(decodedText)
-    #textSample.close()
-    #textToDecode.close()
+    decodedText = decoder(textToDecode,textSample)
+    print(decodedText)
+
+    textSample.close()
+    textToDecode.close()
