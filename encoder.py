6a4c622d6bd0bcf252b3488aa01a955f1db62a72
Alejandro <alejandro-perezg>
2019-11-01 09:18:13 -0600

    FULL COMMENTED

--- encoder.py
+++ encoder.py
@@ -6,18 +6,17 @@ def encode(text, s):
         char = text[i]
 
         if (char.isupper()):
-            #ord() regresa el valor unicode, el contrario al metodo char
-            crypt += chr((ord(char) + s - 65) % 26 + 65)
+            #ord() regresa el valor unicode, el contrario al metodo char de acuerdo a los valores ASCII
+            #26 ES EL NUMERO TOTAL DE LETRAS EN EL ALFABETO
+            crypt += chr((ord(char) + s - 65) % 26 + 65) # El numero 65 indica el valor para la 'A'mayuscula, el cual nos sirve de
+                                                         # limite para las letras mayusculas
         elif(char == " "):
+            #Evita que lea el valor unicode de un espacio y lo añade directamente
 
-            #Evita que lea el valor unicode de un espacio y lo pon
             crypt += " "
-        elif(char == ","):
-            crypt += ","
-        elif (char == "."):
-            crypt += "."
         else:
-            crypt += chr((ord(char) + s - 97) % 26 + 97)
+            crypt += chr((ord(char) + s - 97) % 26 + 97) # El numero 97 indica el valor para la 'a' minuscula, el cual nos sirve de
+                                                         # limite para las letras minusculas
 
     #Transforma en minuscula
     return crypt.lower()
